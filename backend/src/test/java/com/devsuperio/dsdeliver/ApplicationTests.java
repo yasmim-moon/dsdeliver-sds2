98e30f7cb1621e1bf82d3a20c0bbe52963b93de1
Yasmim F <yasmimfernandeslys@gmail.com>
2021-01-05 12:32:19 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperio/dsdeliver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperio.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
