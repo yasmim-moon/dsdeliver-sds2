0a087a07c171968e1d49f0ca884d4700c9c8c293
Yasmim F <yasmimfernandeslys@gmail.com>
2021-01-08 23:55:09 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,16 @@
 import './App.css';
 import React from 'react';
 import Navbar from './Navbar'
+import Home from './Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+
+
+    </>
   );
 }
 
