c2fcc06912a8863e644eefa12bbdf756ace31865
Yasmim F <yasmimfernandeslys@gmail.com>
2021-01-10 12:33:38 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
-
+import ProductsList from './ProductsList';
+import { Product} from '.types';
+import { fetcProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import { OrderLocationdata } from './types';
 
 function Orders(){
+    const [products, setProduct] = useState<Product[]>([]);
+    const [orderLocation, setOrderLOcation] = useState<OrderLocationdata>();
+    useEffect(() => {
+        fetcProducts()
+        .then(response =>  setProduct(response.data))//mudando o valor da lista de produtos
+        .catch(error => console.log(error))
+    },[]);
+    //estado que armazena lista de produtos
     return(
         <div className="orders-container">
             <StepsHeader />
-        
+            <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
 
     
---
+++ front-web/src/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const  API_URL = 'http://localhost:8080';
+//requisação nessa url que irá buscar os produtos
+const mapboxToken = process.env.REACT_APP_ACESS_TOKEN_MAP_BOX;
+
+export function fetcProducts(){
+    return axios(`${API_URL}/products`)
+    //conctenar uma variavel com uma string
+}
+
+export function fetchLocalMapBox(local: string){
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
